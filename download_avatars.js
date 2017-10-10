a38ab3e16d964b0957df6f7d5938ef88df64f4f5
lankypete <lankypete7@gmail.com>
2017-10-10 05:37:12 +0000

    Seems to work

--- download_avatars.js
+++ download_avatars.js
@@ -4,7 +4,7 @@ request = require('request');
 
 
 console.log(`      \u2502\u2592\u2502 \/\u2592\/\r\n      \u2502\u2592\u2502\/\u2592\/\r\n      \u2502\u2592 \/\u2592\/\u2500\u252C\u2500\u2510\r\n      \u2502\u2592\u2502\u2592|\u2592\u2502\u2592\u2502\r\n      \u250C\u2534\u2500\u2534\u2500\u2510-\u2518\u2500\u2518 \r\n      \u2502\u2592\u250C\u2500\u2500\u2518\u2592\u2592\u2592\u2502\r\n      \u2514\u2510\u2592\u2592\u2592\u2592\u2592\u2592\u250C\u2518\r\n      \u2514\u2510\u2592\u2592\u2592\u2592\u250C\u2518\r\n`);
-console.log('Welcome to the GitHub Avatar Downloader!')
+
 
 
 const GITHUB_U = 'lankypete',
@@ -22,19 +22,47 @@ const urlConfig = {
 
 
 function getRepoContributors(author, repo) {
+  console.log('Welcome to the GitHub Avatar Downloader!')
+  console.log('The users avatars of the ' + repo + ' repo will be added to the Avatars directory!')
 
   urlConfig.url = `https://${GITHUB_U}:${GITHUB_T}@api.github.com/repos/${author}/${repo}/contributors`;
 
   request( urlConfig , function(err, res, body){
-    console.log('Errors: ', err);
+    if(err) console.log('Errors: ', err);
 
     const users = JSON.parse(body);
 
-    const userAvatarURLs = users.map(function(a){
-      return a.avatar_url;
+    const userNameAndAvatarURLs = users.map(function(a){
+      const userNameAvatarObj = {
+        userLogin : a.login,
+        avatarURL : a.avatar_url
+      }
+      return userNameAvatarObj;
     })
 
+    userNameAndAvatarURLs.forEach(function(user){
+      const avatarURL = user.avatarURL;
+      const filePath = "./Avatars/" + user.userLogin + '.png';
+      downloadImageByURL(avatarURL, filePath);
+    })
+
+
   });
+
+
+}
+
+
+function downloadImageByURL(imgURL, path) {
+  if (!fs.existsSync('Avatars')){
+      fs.mkdirSync('Avatars');
+  }
+  request(imgURL)
+  .on('error', function(err) { throw err; })
+  .pipe(fs.createWriteStream(path))
+  .on('finish', function(){ console.log('Downloaded an Image!' ); });
+
+
 }
 
 
